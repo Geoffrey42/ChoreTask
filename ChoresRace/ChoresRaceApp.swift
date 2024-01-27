2aa7a0c0899d618f8786aa2a4ef740d21e65f431
Geoffrey Gane <geoffreygane@gmail.com>
2024-01-27 23:49:48 +0100

    Initial Commit

---
+++ ChoresRace/ChoresRaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoresRaceApp.swift
+//  ChoresRace
+//
+//  Created by Geoffrey Gane on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChoresRaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
